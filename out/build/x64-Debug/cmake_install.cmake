4f4e39967dd7cecfe768fa8419b2d58255a18a75
Ozynul <38048@student.atar.edu.pl>
2026-01-21 15:34:03 +0100

    dodano nową mapę, zmiany w statystykach

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/Pliki/gra dungeon projekt/gra-dungeon-crawler
+# Install script for directory: C:/Users/a3non/source/repos/gra-dungeon-crawlerv3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "D:/Pliki/gra dungeon projekt/gra-dungeon-crawler/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/a3non/source/repos/gra-dungeon-crawlerv3/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,18 +34,18 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/Pliki/gra dungeon projekt/gra-dungeon-crawler/out/build/x64-Debug/_deps/glfw-build/cmake_install.cmake")
+  include("C:/Users/a3non/source/repos/gra-dungeon-crawlerv3/out/build/x64-Debug/_deps/glfw-build/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/Pliki/gra dungeon projekt/gra-dungeon-crawler/out/build/x64-Debug/_deps/glm-build/cmake_install.cmake")
+  include("C:/Users/a3non/source/repos/gra-dungeon-crawlerv3/out/build/x64-Debug/_deps/glm-build/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/Pliki/gra dungeon projekt/gra-dungeon-crawler/out/build/x64-Debug/install_local_manifest.txt"
+  file(WRITE "C:/Users/a3non/source/repos/gra-dungeon-crawlerv3/out/build/x64-Debug/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/Pliki/gra dungeon projekt/gra-dungeon-crawler/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/a3non/source/repos/gra-dungeon-crawlerv3/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
